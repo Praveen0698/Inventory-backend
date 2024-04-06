8ac22be822c63e88e13b7e717785b86e961bafe5
praveen <kpking900@gmail.com>
2024-04-06 11:54:38 +0530

    first-commit

---
+++ messagin/Messaging.js
@@ -0,0 +1,52 @@
+const twilio = require("twilio");
+
+const accountSid = process.env.ACCOUNTSID;
+const authToken = process.env.AUTHTOKEN;
+const client = twilio(accountSid, authToken);
+
+exports.messageCreate = async (req, res) => {
+  const { mobileNo, id } = req.body;
+  const billLink = `http://localhost:3000/bills/${id}`;
+
+  client.messages
+    .create({
+      body: `Thanks for shopping!!
+      Here is your bill: ${billLink}`,
+      from: "+17868411057",
+      to: `+91${mobileNo}`,
+    })
+    .then(() => {
+      res.status(200).send("Bill link sent successfully!");
+    })
+    .catch((err) => {
+      console.error("Error sending SMS:", err);
+      res.status(500).send("Failed to send bill link");
+    });
+};
+
+// const twilio = require("twilio");
+
+// const accountSid = process.env.ACCOUNTSID;
+// const authToken = process.env.AUTHTOKEN;
+// const client = twilio(accountSid, authToken);
+
+// exports.messageCreate = async (req, res) => {
+//   try {
+//     const { excelData } = req.body;
+
+//     await Promise.all(
+//       excelData.map((number, index) => {
+//         return client.messages.create({
+//           body: `Thanks for Voting!!`,
+//           from: "+19382223956",
+//           to: `+91${number.MOBILE}`,
+//         });
+//       })
+//     );
+
+//     res.status(200).send("Bill link sent successfully!");
+//   } catch (err) {
+//     console.error("Error sending SMS:", err);
+//     res.status(500).send("Failed to send bill link");
+//   }
+// };
